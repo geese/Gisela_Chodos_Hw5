7196a341b5b726b4440dacdc03ab2093f5ab0a14
Gisela Chodos <gisela.chodos@gmail.com>
2016-10-29 15:01:48 -0600

    Adding a skeleton document.

---
+++ Gisela_Chodos_Hw5.py
@@ -0,0 +1,39 @@
+"""
+Prompt user for a correct PIN no more than three times.
+Usage:
+    python3 Gisela_Chodos_Hw5.py
+"""
+#!/usr/bin/env python
+import sys
+
+def getInput():
+    """
+    Prompt user for PIN and read keyboard input.
+    Prints error message if:
+        PIN contains non-numeric character(s),
+        PIN does not contain exactly four digits,
+        PIN is not 1234.
+    Args:
+        None
+    Returns:
+        True, if the PIN is correct.
+    """
+    pass
+
+
+# Main function
+def main():
+    """
+    Prompt user for PIN no more than 3 times.
+    Print "Your bank card is blocked" after three incorrect PIN attempts.
+    Args:
+        None
+    """
+    pass
+
+if __name__ == '__main__':
+    # Call Main
+    main()
+    exit(0)
+
+
